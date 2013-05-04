d5e7f2f6180396851a815610b999f4475ba0cd15
Wesley Tansey <wes.tansey@gmail.com>
2013-05-04 15:10:59 -0500

    Filled in robust response code

--- robust_response.py
+++ robust_response.py
@@ -10,13 +10,27 @@ from skew_agents import *
 
 strat_file = sys.argv[1]
 response_player = int(sys.argv[2])
+fixed_player = -1 * response_player + 1
 prob_fixed = float(sys.argv[3])
 iterations = int(sys.argv[4])
 out_file = sys.argv[5]
 
 leduc = leduc_rules()
+gametree = GameTree(leduc)
+gametree.build() 
 
-cfr = RestrictedNashResponse(leduc, response_player, fixed_strategy_profile, prob_fixed)
+strategies = [None, None]
+fixed_strat = Strategy(fixed_player)
+fixed_strat.load_from_file(strat_file)
+strategies[fixed_player] = fixed_strat
+
+response_strat = Strategy(response_player)
+response_strat.build_default(gametree)
+strategies[response_player] = response_strat
+
+profile = StrategyProfile(leduc, strategies)
+
+cfr = RestrictedNashResponse(leduc, response_player, profile, prob_fixed)
 
 iterations_per_block = 100
 blocks = iterations / iterations_per_block
